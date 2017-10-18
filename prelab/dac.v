15b3921629b68f3a2729c5a0ac2bbea0dd0149a1
Vishnu Gottiparthy <vishnu.gottiparthy@gmail.com>
2017-10-18 17:53:51 -0400

    Update dac.v

--- prelab/dac.v
+++ prelab/dac.v
@@ -1,5 +1,5 @@
 /* 1. Divide by 51 since there are 51 values between 0.0 and 5.0 inclusive incrementing by 0.1
- * 2. Numerator width: must be 8 bits to accommodate 8 bit input
+ * 2. Numerator width: must be 9 bits to accommodate 8 bit input times 10
  * 3. Denominator: only need 7 bits since we only divide by 51
  * 4. 3.984V would give a simpler design since 255/3.984 = 64, which is a power of two. This 
       means that each division could be accomplished with a right shift. 
@@ -12,12 +12,10 @@ module dac(d, ones_digit, tenths_digit);
 	input[7:0] d;
 	output[7:0] ones_digit, tenths_digit;
 
-	wire[7:0] remainder, hundr_digit;
+	wire[7:0] remainder;
 
 	lpm_divider dividerOnes(.denom(7'd51), .numer(d), .quotient(ones_digit), .remain(remainder));
 
-	lpm_divider dividerTenths(.denom(7'd51), .numer(remainder), .quotient(tenths_digit));
-
-	assign tenths_digit = (8'd10)*hundr_digit;
+	lpm_divider dividerTenths(.denom(7'd51), .numer(remainder*(9'd10)), .quotient(tenths_digit));
 
 endmodule //dac
